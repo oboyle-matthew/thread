33c5ad8880fd1a6175e9a0df10d3812bc623a5dd
Matthew O'Boyle <oboyle.matthew@duke.edu>
2019-02-10 19:55:03 -0500

    Reading in files as arguments

--- p1d/deli.cc
+++ p1d/deli.cc
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <fstream>
 #include<iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+	cout << "You have entered " << argc-1 << " arguments.\n";
 	cout << "Hello World!\n";
-	return 0;
+	string line;
+	for (int i = 1; i < argc; i++) {
+		cout << "Attempting to read file " << argv[i] << ":\n";
+		ifstream myfile (argv[i]);
+	  	if (myfile.is_open()) {
+	    		while (getline(myfile,line)) {
+	      			cout << line << '\n';
+	   		 }
+	   		 myfile.close();
+	  	} else {
+			cout << "Unable to open file\n";
+		}
+		cout << "\n\n";
+	}
+	
 }
